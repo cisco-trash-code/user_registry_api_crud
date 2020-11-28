a6c7290685cca9f56136ad327c7224d060cb1ce3
cisco <sovisrushain@gmail.com>
2020-11-28 09:58:05 +0530

    Finish Header component.

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 const Header = () => {
     return ( 
-        <div className='row row-1'>
-            <h1>Header</h1>
+        <div>
+            <div className='row row-1'>
+                <h1 className='mx-auto m-5'>User Registery</h1>
+            </div>
+            <form>
+                <div class="form-row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Name" />
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Username" />
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="email" />
+                    </div>
+                </div>
+                <div class="form-row mt-3">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="City" />
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Phone" />
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Company" />
+                    </div>
+                </div>
+            </form>
         </div>
      );
 }
