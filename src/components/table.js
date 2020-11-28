324f799fc806feb6dd3d45904f9bf3c50a83edab
cisco <sovisrushain@gmail.com>
2020-11-28 09:47:56 +0530

    Implement bascic UI skeleton.

---
+++ src/components/table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Table = () => {
+    return ( 
+        <div className='row row-2'>
+            <h1>Table</h1>
+        </div>
+     );
+}
+ 
+export default Table;
